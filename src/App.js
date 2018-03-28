5d42f1971c732795c189156e1667f51542df0b9e
ZachMoore <Get3Moore@gmail.com>
2018-03-28 10:42:48 -0400

    add search functionality

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class BooksApp extends React.Component {
       books: []
     };
 
-    this.moveBook = (bookToMove, dest) => {
+    this.moveBook = (bookToMove, dest, add = false) => {
       this.setState(old => {
         return {books: old.books.map(book => {
           if (book.id === bookToMove.id) {
--- src/searchPage.js
+++ src/searchPage.js
@@ -1,45 +1,61 @@
 
 import React from "react"
 import { Link } from "react-router-dom"
+import * as BooksAPI from './BooksAPI'
+import Book from "./book.js"
 
 class searchPage extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+          searchResults: [],
+          searchText: ""
+        };
+        this.getBooks = (event) => {
+            this.setState({searchText: event.target.value})
+            if (event.target.value.length > 0){
+                BooksAPI.search(event.target.value).then(res => {
+                    console.log(res);
+                    if (res && !res.error) {
+                        let filtered = res.map((book) =>{
+                            if (!book.imageLinks){
+                                console.log(book)
+                                book.imageLinks.thumbnail = "http://via.placeholder.com/128x192"
+                            }
+                            return book
+                            
+                        })
+                        this.setState({searchResults: filtered})
+                    }
+                })
+            } else {
+                this.setState({searchResults: []})
+            }
+            
+        }
+      }
+
     render() {
+        console.log(this.state.searchText)
         return (
             <div className="search-books">
                 <div className="search-books-bar">
                     <Link className="close-search" to="/">Close</Link>
                     <div className="search-books-input-wrapper">
-                    {/*
-                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                        You can find these search terms here:
-                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                        you don't find a specific author or title. Every search is limited by search terms.
-                    */}
-                    <input type="text" placeholder="Search by title or author"/>
-
+                    <input type="text" placeholder="Search by title or author" value={this.state.searchText} onChange={(event) => {
+                        this.getBooks(event)
+                    }} />
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        {this.state.searchResults.map((book) => <Book book={book} key={book.id} moveBook={this.props.moveBook} />)}
+                    </ol>
                 </div>
             </div>
         )
     }
 }
-
-// constructor(props){
-//     super(props);
-//     this.state = {
-//       searchResults: [],
-//       searchText: ""
-//     };
-//     this.getBooks = (event) => {
-//         this.setState({searchText: event.target.value})
-//         BooksAPI.search(this.state.searchText).then(res => this.setState({searchResults: res}))
-//     }
-//   }
 
 export default searchPage
